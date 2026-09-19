ec4df01961a51d1893ff4c46ce03517d621f1cba
Ceres <161601649+ceres-wl@users.noreply.github.com>
2026-09-19 17:02:35 -0300

    Configuração inicial

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+    let processFile = $state(null);
+    let configFile = $state(null);
+</script>
+
+<input bind:files={processFile} type="file" name="processos">
+<input bind:files={configFile} type="file" name="config">
---
+++ src/lib/types.ts
@@ -0,0 +1,4 @@
+export interface Config {
+    quantum: number,
+    aging: number
+}
